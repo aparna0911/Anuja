6798fd27ec10048751ae2e152e246ba049edeee4
apple <apple@192.168.1.100>
2022-07-19 20:22:20 +0530

    [Anuja] : Added 2 demo classes.

---
+++ src/Demo_GitHub/Demo1.java
@@ -0,0 +1,11 @@
+package Demo_GitHub;
+
+public class Demo1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Good Night!!");
+		
+	}
+
+}
---
+++ src/Demo_GitHub/Demo2.java
@@ -0,0 +1,11 @@
+package Demo_GitHub;
+
+public class Demo2 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Good Morning!!");
+		
+	}
+
+}
